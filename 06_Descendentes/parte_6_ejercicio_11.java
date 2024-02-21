22c97a1fa59017ad798f5b2bc9ab44c2322938b6
Marcelo Gelato <gelatomarcelo@hotmail.com>
2024-02-20 22:22:13 -0300

    ejercicio 11 agregado

---
+++ 06_Descendentes/parte_6_ejercicio_11.java
@@ -0,0 +1,14 @@
+/*Escribir un programa que mientras el usuario ingrese un número entero
+entre 1 y 10 realice:
+– Si el numero ingresado es múltiplo de 3 pida ingresar un caracter, y para el caracter
+ingresado imprima a que tipo de carácter esta asociado:
+• “letra minúscula” si el caracter es una letra del abecedario en minúscula;
+• “letra mayúscula” si el caracter es una letra del abecedario en mayúscula;
+• “dígito” si el caracter corresponde a un carácter número;
+• “otro” para los restantes casos de caracteres.
+– Si el numero ingresado es múltiplo de 5 imprima la tabla de multiplicar del numero
+ingresado.*/
+
+public class parte_6_ejercicio_11 {
+
+}
