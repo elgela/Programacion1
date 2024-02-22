35b111828e887390c3c0113bf97026d874c255f6
Marcelo Gelato <gelatomarcelo@hotmail.com>
2024-02-22 11:05:48 -0300

    no terminado

--- 06_Descendentes/parte_6_ejercicio_11.java
+++ 06_Descendentes/parte_6_ejercicio_11.java
@@ -9,8 +9,58 @@ ingresado imprima a que tipo de carácter esta asociado:
 – Si el numero ingresado es múltiplo de 5 imprima la tabla de multiplicar del numero
 ingresado.*/
 
+import java.io.*;
+
 public class parte_6_ejercicio_11 {
+    public static final int MIN = 1;
+    public static final int MAX = 10;
+
     public static void main(String[] args) {
-        
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+
+        int numero;
+        char caracter;
+
+        try {
+            System.out.println("Ingrese un número entero entre 1 y 10");
+            numero = Integer.valueOf(entrada.readLine());
+
+            while ((numero >= MIN) && (numero <= MAX)) {
+                if (numero % 3 == 0) {
+                    System.out.println("Ingrese un carácter");
+                    caracter = entrada.readLine().charAt(0);
+
+                    tipo_caracter(caracter);
+                    System.out.println('\0');
+
+                    System.out.println("Ingrese un carácter");
+                    caracter = entrada.readLine().charAt(0);
+
+                } else if (numero % 5 == 0) {
+
+                }
+            }
+        } catch (Exception exc) {
+            System.out.println(exc);
+        }
+    }
+
+    public static void tipo_caracter(char caracter) {
+        if ((caracter >= '1') && (caracter <= '9')) {
+            System.out.println("Dígito");
+            System.out.println('\0');
+        }
+        else if ((caracter >= 'a') && (caracter <= 'z')) {
+            System.out.println("Letra minúscula");
+            System.out.println('\0');
+        }
+        else if ((caracter >= 'A') && (caracter <= 'Z')) {
+            System.out.println("Letra mayúscula");
+            System.out.println('\0');
+        }
+        else {
+            System.out.println("Otro");
+            System.out.println('\0');
+        }
     }
 }
