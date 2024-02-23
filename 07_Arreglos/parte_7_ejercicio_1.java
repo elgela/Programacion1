653bbcf64d17dc0838f5be5171eafd60674fda53
Marcelo Gelato <gelatomarcelo@hotmail.com>
2024-02-23 00:55:13 -0300

    Empezado

---
+++ 07_Arreglos/parte_7_ejercicio_1.java
@@ -0,0 +1,8 @@
+/*Hacer un programa que dado un arreglo de enteros de tamaño 10 que se encuentra
+precargado, invierta el orden del contenido (por ejemplo: el que está en 0 se intercambia con
+el que está en 9, el que está en 1 con el que está en 8...). Este intercambio no se debe realizar
+de manera explícita, hay que hacer un método que incluya una iteración de intercambio.*/
+
+public class parte_7_ejercicio_1 {
+
+}
