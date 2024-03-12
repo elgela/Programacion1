a4f3969bf08c2186cc76cd25c969d3096498bdf6
Marcelo Gelato <gelatomarcelo@hotmail.com>
2024-03-12 17:38:34 -0300

    ejercicio_4 empezado

---
+++ 07_Arreglos/parte_7_ejercicio_4.java
@@ -0,0 +1,9 @@
+/*Hacer un programa que dado un arreglo de enteros de tamaño 10 que se encuentra
+precargado, solicite al usuario un numero entero y lo agregue al principio del arreglo (posición
+0). Para ello tendrá que realizar un corrimiento a derecha (se pierde el último valor del arreglo) y
+colocar el numero en el arreglo en la posición indicada.*/
+
+import java.util.Random;
+public class parte_7_ejercicio_4 {
+
+}
